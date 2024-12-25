0cd4ce03ae3086adc7a651b5384f1f6da8e31850
Victor Lanciu <wlanciu@gmail.com>
2024-12-25 16:09:54 +0200

    Added  second problem

---
+++ Problema 2/Angajat.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_2
+{
+    public class Angajat
+    {
+        public string Nume { get; set; }
+        public Angajat (string nume)
+        {
+            Nume = nume;
+        }
+        public virtual void AfisareDetalii() { Console.WriteLine($"Numele angajatului este {Nume}"); }
+        
+    }
+    public class Manager : Angajat
+    {
+        public string Departament { get; set; }
+        public Manager(string nume, string departament) :base(nume)
+        {
+            Departament = departament;
+        }
+        public override void AfisareDetalii()
+        {
+            base.AfisareDetalii();
+            Console.WriteLine($"Departamentul {Departament}");
+        }
+
+    }
+}
---
+++ Problema 2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Problema_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager person = new Manager("Vitalie", "IT");
+            person.AfisareDetalii();
+        }
+
+    }
+}
