d60f4dbb22910f31d4ea47672be98a80aa6c36ad
Victor Lanciu <wlanciu@gmail.com>
2024-12-25 15:53:28 +0200

    Added first problem

---
+++ Zoo/Animal.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo
+{
+    public interface Irunnable
+    {
+        void Run();
+    }
+    public abstract class Animal
+    {
+        public abstract void MakeSound();
+        public virtual void Eat()
+        {
+            Console.WriteLine("Animalul mananca");
+        }
+
+    }
+    public class Dog : Animal, Irunnable
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Cainele latra");
+        }
+        public override void Eat()
+        {
+            Console.WriteLine("Cainele mananca oase");
+        }
+        public void Run()
+        {
+            Console.WriteLine("Dog runs");
+        }
+    }
+    public class Cat : Animal, Irunnable
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Pisica miauna");
+        }
+        public override void Eat()
+        {
+            Console.WriteLine("Pisica mananca peste");
+        }
+        public void Run()
+        {
+            Console.WriteLine("Cat runs");
+        }
+    }
+}
---
+++ Zoo/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Zoo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal cat = new Cat();
+            Animal dog = new Dog();
+            cat.Eat();
+            cat.MakeSound();
+            dog.Eat();
+            dog.MakeSound();
+            if (dog is Irunnable runableDog)
+            {
+                runableDog.Run();
+            }
+            if (cat is Irunnable runableCat)
+            {
+                runableCat.Run();
+            }
+
+        }
+    }
+}
