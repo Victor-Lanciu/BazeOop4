a27ca2cde68a1b182254cd8876aae986661eb7e0
Victor Lanciu <wlanciu@gmail.com>
2024-12-25 16:27:09 +0200

    Added third probem.

---
+++ Problema 3/Animal.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problema_3
+{
+    public class Animal
+    {
+        public string Nume { get; set; }
+        public virtual void AfisareDetalii()
+        {
+            Console.WriteLine($"Denumirea animalului: {Nume}");
+        }
+    }
+    public class Mamifer : Animal
+    {
+        public string CuloareBlana { get; set; }
+        public override void AfisareDetalii()
+        {
+            base.AfisareDetalii();
+            Console.WriteLine($"Culoarea blanii: {CuloareBlana}");
+        }
+    }
+    public class Pasare : Animal
+    {
+        public string TipZbor { get; set; }
+        public override void AfisareDetalii()
+        {
+            base.AfisareDetalii();
+            Console.WriteLine($"Tipul zborului: {TipZbor}");
+        }
+    }
+    public class Papagal : Pasare
+    {
+        public int Vocabular { get; set; }
+        public override void AfisareDetalii()
+        {
+            
+            base.AfisareDetalii();
+            Console.WriteLine($"Cuvinte stiute: {Vocabular}");
+        }
+    }
+
+}
---
+++ Problema 3/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Problema_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mamifer tigru = new Mamifer();
+            tigru.Nume = "Tigru";
+            tigru.CuloareBlana = "Roscat-Alba cu elemente negre";
+
+            Pasare bufnita = new Pasare();
+            bufnita.Nume = "Bufnita";
+            bufnita.TipZbor = "Silentios";
+
+            Papagal papagal = new Papagal();
+            papagal.Nume = "Perus";
+            papagal.TipZbor = "Rapid si agil";
+            papagal.Vocabular = 12;
+
+            tigru.AfisareDetalii();
+            bufnita.AfisareDetalii();
+            papagal.AfisareDetalii();
+        }
+    }
+}
